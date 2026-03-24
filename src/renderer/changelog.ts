fca2bfd78931ab506049659974dbabd2cf345e2e
nubbymong <192402368+nubbymong@users.noreply.github.com>
2026-03-24 07:52:42 +0000

    Release v1.2.142

--- src/renderer/changelog.ts
+++ src/renderer/changelog.ts
@@ -15,7 +15,7 @@ export interface ChangelogEntry {
 
 export const changelog: ChangelogEntry[] = [
   {
-    version: '1.2.141',
+    version: '1.2.142',
     date: '2026-02-11',
     highlights: "Maintenance release with internal improvements",
     changes: [
