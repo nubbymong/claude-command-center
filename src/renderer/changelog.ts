e68be53b919b1e820deee2da2a0548164e2a16b7
nicholas-moger <nicholas.moger@jpmorgan.com>
2026-04-05 04:39:07 +0100

    Release v1.2.157

--- src/renderer/changelog.ts
+++ src/renderer/changelog.ts
@@ -15,7 +15,7 @@ export interface ChangelogEntry {
 
 export const changelog: ChangelogEntry[] = [
   {
-    version: '1.2.156',
+    version: '1.2.157',
     date: '2026-02-11',
     highlights: "Maintenance release with internal improvements",
     changes: [
